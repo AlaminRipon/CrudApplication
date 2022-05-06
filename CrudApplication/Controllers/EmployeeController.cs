b5f64a581de18e4fb1a62278194f5a8742555e3a
AlaminRipon <alaminripon5654@gmail.com>
2022-05-06 08:56:38 +0600

    Add project files.

---
+++ CrudApplication/Controllers/EmployeeController.cs
@@ -0,0 +1,21 @@
+﻿using CrudApplication.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace CrudApplication.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly ApplicationContext context;
+
+        public EmployeeController(ApplicationContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            var result = context.Employees.ToList();
+            return View(result);
+        }
+    }
+}
---
+++ CrudApplication/Data/ApplicationContext.cs
@@ -0,0 +1,14 @@
+﻿using CrudApplication.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudApplication.Data
+{
+    public class ApplicationContext: DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
